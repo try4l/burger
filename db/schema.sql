b983c986cd86535303e99ae390c6ac9729bb8784
Alan Davison <abdavison@cox.net>
2017-11-13 15:54:55 -0800

    Changing SQL code to try to fix Heroku deploy error.

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,6 @@
-DROP DATABASE IF EXISTS `burgers_db`;
-CREATE DATABASE `burgers_db`;
+CREATE DATABASE IF NOT EXISTS `burgers_db`;
 USE `burgers_db`;
-DROP TABLE IF EXISTS `burgers`;
-CREATE TABLE `burgers`(
+CREATE TABLE IF NOT EXISTS `burgers`(
 `id` INTEGER(10) NOT NULL primary key auto_increment,
 `burger_name` VARCHAR(100) NOT NULL,
 `devoured` BOOL DEFAULT false,
